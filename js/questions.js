027160287b0d35eee1b84d5cf21ef6e5e9f46bd8
dnptrck <dnptrck11@gmail.com>
2021-11-03 03:29:28 +0800

    1st

---
+++ js/questions.js
@@ -0,0 +1,119 @@
+let MCQS = [{
+     question: "Below are examples of communication situations EXCEPT?",
+    choice1: "Feedback",
+    choice2: "Decoding",
+    choice3: "Encoding",
+    choice4: "Communication",
+    answer: 1
+},
+            {
+            question: "It  refers to the way or mode the message flows or is transmitted through. ",
+    choice1: "Message",
+    choice2: "Noise",
+    choice3: "Channel",
+    choice4: "Feedback",
+    answer: 2
+},
+            {
+           question: "It can help senders know if their message was received and understood.",
+    choice1: "Noise",
+    choice2: "Feedback",
+    choice3: "Clarity",
+    choice4: "Channel",
+    answer: 1
+},
+            {
+            question: "This model also called Schramm Model.",
+    choice1: "Interactive Model",
+    choice2: "Transactional Model",
+    choice3: "Model of Communication",
+    choice4: "Linear Model",
+    answer: 0
+},
+            {
+            question: "Who original developed Linear Model of communication?",
+    choice1: "Wilbur Schramm",
+    choice2: "Shannon and Weaver",
+    choice3: "Dean Barnlund",
+    choice4: "Frank Dance",
+    answer: 1
+},
+            {
+            question: "Below are examples of Transactional Model of communication EXCEPT?",
+    choice1: "Telephone Call",
+    choice2: "Skype Call",
+    choice3: "Broadcasting",
+    choice4: "Interactive Training",
+    answer: 2
+},
+            {
+               question: "It is a two way process and the sender received a feedback from the receiver.",
+    choice1: "Linear Model ",
+    choice2: "Transactional Model ",
+    choice3: "Decoder ",
+    choice4: "Interactive Model ",
+    answer: 3
+},{
+               question: "The transmitter which converts the message into signals.",
+    choice1: "Linear Model",
+    choice2: "Interactive Model",
+    choice3: "Encoder",
+    choice4: "Decoder",
+    answer: 2
+},
+            {
+               question: "Check your tone of voice, pronunciation, and speech rate.",
+    choice1: "Keep Focused",
+    choice2: "Minimize Distraction",
+    choice3: "Speak Intelligibly",
+    choice4: "Be Specific",
+    answer: 2
+},
+            {
+               question: "Function to motivate or to inspire someone by your speech or advice.",
+    choice1: "Motivation",
+    choice2: "Regulation or Control",
+    choice3: "Emotional Expression",
+    choice4: "Information Dissemination",
+    answer: 0
+},
+            {
+               question: "Use simple and not confusing words to your audience.",
+    choice1: "Speak Intelligibly",
+    choice2: "Minimize Distraction",
+    choice3: "Be Specific",
+    choice4: "Listen with your Eyes and Ears",
+    answer: 2
+},           
+			{
+            question: "You should listen what people say and act.",
+    choice1: "Speak Intelligibly",
+    choice2: "Minimize Distraction",
+    choice3: "Listen with your Eyes and Ears",
+    choice4: "Do not Jump to Conclusion",
+    answer: 2
+},
+            {
+               question: "You must based on facts, evidences, and reliable information.",
+    choice1: "Do not Jump to Conclusion",
+    choice2: "Listen with your Eyes and Ears",
+    choice3: "Keep Focused",
+    choice4: "Minimize Distraction",
+    answer: 3
+}, 
+            {
+            question: "You should state your purpose of communication.",
+    choice1: "Intrapersonal Communication",
+    choice2: "Interpersonal Communication",
+    choice3: "Public Communication",
+    choice4: "Mass Communication",
+    answer: 1
+},
+            {
+            question: "It pertains to use suitable words to deliver message or speech.",
+    choice1: "Brevity",
+    choice2: "Ethics",
+    choice3: "Appropriateness",
+    choice4: "Vividness",
+    answer: 2
+}];
